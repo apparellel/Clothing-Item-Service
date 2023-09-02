d07de2f574b764fb7ea7a4c1f5c4a4607c46fe79
pratishakaranth <pratishakaranth100@gmail.com>
2023-09-02 18:27:32 +0530

    feat: add springboot project

---
+++ src/test/java/com/apparallel/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apparallel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
